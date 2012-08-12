3f26fa78eb1c2640fe10ef4899f6e4bb2c40c83d
Kevin Horvath <kevinho@ubuntu.(none)>
2012-08-11 20:19:41 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitHub::Application.config.secret_token = 'd83ad021d5540ab503538400863fe83ee65c1185d053d4acf73ca36f574118b5bda5a98acdacb27a4271e78b00519c1a96578c3bb8d12d1299f2123da99a9ab1'
